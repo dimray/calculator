f83d3fe4db54c3bf2a611ad058df8eb33fa145fe
dimray <dimray@gmail.com>
2022-10-19 20:23:49 +0100

    worked out calculator logic

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
 function operate(a, b, operator) {
 
     const operators = {
-        'add': (a, b) => a + b,
-        'subtract': (a, b) => a - b,
-        'multiply': (a, b) => a * b,
-        'divide': (a, b) => a / b,
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '*': (a, b) => a * b,
+        '/': (a, b) => a / b,
     }
 
     return operator in operators ? operators[operator](a, b) : NaN;
 }
 
+const buttons = document.querySelectorAll(".button");
+buttons.forEach(function (button) {
+    button.addEventListener("click", function () {
 
-const display = document.querySelector("#display");
-display.value = "123456";
+        let buttonSelected = button.textContent;
+        // display.value = buttonSelected;
 
-const numbers = document.querySelectorAll(".number");
-numbers.forEach(function (number) {
-    const numbClick = number.addEventListener("click", function () {
-        console.log(number.textContent);
+        if (numbers.includes(+buttonSelected) || buttonSelected === ".") {
+            numberArray.push(buttonSelected);
+            display.value = numberArray.join("");
+        } else if (a === "") {
+            a = +numberArray.join("");
+            numberArray = [];
+            operator = buttonSelected;
+        } else if (b === "") {
+            b = +numberArray.join("");
+            display.value = operate(+a, +b, operator);
+            a = operate(+a, +b, operator);
+            numberArray = [];
+        } else {
+            operator = buttonSelected;
+            b = "";
+        }
     });
 
-})
+
+});
+
+
+
+
+
+
+const display = document.querySelector("#display");
+display.value = "";
+
+let numberArray = [];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let operator = "";
+let a = "";
+let b = "";
+
+
+
