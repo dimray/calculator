53f752af3cdc97c16414f6b87f97bdfe6db79105
dimray <dimray@gmail.com>
2022-10-20 21:02:19 +0100

    completed calculator

--- script.js
+++ script.js
@@ -24,76 +24,87 @@ function operate(a, b, operator) {
     return operator in operators ? operators[operator](a, b) : NaN;
 }
 
+document.addEventListener("keydown", function (event) {
+    let buttonSelected = event.key;
+    if (event.key === "Enter") { buttonSelected = "=" };
+    if (event.key === "Escape") { buttonSelected = "AC" } ''
+
+    calculate(buttonSelected);
+
+})
+
+
+
 buttons.forEach(function (button) {
     button.addEventListener("click", function () {
-
         let buttonSelected = button.textContent;
 
-
-
-        if (numbers.includes(+buttonSelected) || buttonSelected === ".") {
-            numberArray.push(buttonSelected);
-            display.value = numberArray.join("");
-        };
-
-        if (operatorSymbols.includes(buttonSelected) || buttonSelected === "=") {
-
-            if (a === "") {
-                if (numberArray.length > 0) { a = +numberArray.join("") };
-                numberArray = [];
-                if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected }
-            } else if (b === "") {
-                if (numberArray.length > 0) {
-                    b = +numberArray.join("");
-                    numberArray = [];
-                    answer = operate(a, b, operator);
-                    display.value = answer;
-                    a = answer;
-                    if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected }
-                } else {
-                    if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected }
-                }
-            } else {
-                if (numberArray.length > 0) {
-                    b = +numberArray.join("");
-                    numberArray = [];
-                    answer = operate(a, b, operator);
-                    display.value = answer;
-                    a = answer;
-                    if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected };
-                } else {
-                    if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected };
-                }
-
-            }
-        } else if (buttonSelected === "AC") {
-            a = "";
-            b = "";
-            operator = ""
-            numberArray = [];
-            display.value = "";
-            answer = "";
-        } else if (buttonSelected === "+/-") {
-            if (numberArray.length > 0) {
-                numberArray.unshift("-");
-                display.value = numberArray.join("");
-            } else {
-                a = +a * -1;
-                display.value = a;
-            }
-        } else if (buttonSelected === "%") {
-            if (numberArray.length > 0) {
-                value = numberArray.join("") / 100;
-                value = value.toString();
-                numberArray = value.split("");
-                display.value = numberArray.join("");
-            } else {
-                a = +a / 100;
-                display.value = a;
-            }
-        }
+        calculate(buttonSelected);
     });
-
-
 });
 
+function calculate(buttonSelected) {
+
+    if (numbers.includes(+buttonSelected) || buttonSelected === ".") {
+        numberArray.push(buttonSelected);
+        display.value = numberArray.join("");
+    };
+
+    if (operatorSymbols.includes(buttonSelected) || buttonSelected === "=") {
+
+        if (a === "") {
+            if (numberArray.length > 0) { a = +numberArray.join("") };
+            numberArray = [];
+            if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected }
+        } else if (b === "") {
+            if (numberArray.length > 0) {
+                b = +numberArray.join("");
+                numberArray = [];
+                answer = operate(a, b, operator);
+                display.value = answer;
+                a = answer;
+                if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected }
+            } else {
+                if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected }
+            }
+        } else {
+            if (numberArray.length > 0) {
+                b = +numberArray.join("");
+                numberArray = [];
+                answer = operate(a, b, operator);
+                display.value = answer;
+                a = answer;
+                if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected };
+            } else {
+                if (operatorSymbols.includes(buttonSelected)) { operator = buttonSelected };
+            }
+
+        }
+    } else if (buttonSelected === "AC") {
+        a = "";
+        b = "";
+        operator = ""
+        numberArray = [];
+        display.value = "";
+        answer = "";
+    } else if (buttonSelected === "+/-") {
+        if (numberArray.length > 0) {
+            numberArray.unshift("-");
+            display.value = numberArray.join("");
+        } else {
+            a = +a * -1;
+            display.value = a;
+        }
+    } else if (buttonSelected === "%") {
+        if (numberArray.length > 0) {
+            value = numberArray.join("") / 100;
+            value = value.toString();
+            numberArray = value.split("");
+            display.value = numberArray.join("");
+        } else {
+            a = +a / 100;
+            display.value = a;
+        }
+    }
+
+}
